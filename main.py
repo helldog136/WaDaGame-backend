347014baa8126270b4d9aa1a0c0f02937d0ad60c
Duncan De Weireld <duncan.deweireld@student.umons.ac.be>
2017-03-11 01:49:29 +0100

    began rest routes

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/<appId>', methods = ['POST'])
+def addImage(appId):
+    if request.headers['Content-Type'] == 'application/octet-stream':
+        f = open('./appID.jpg', 'wb')
+        f.write(request.data)
+        f.close()
+
+
+app.run(port=4242)
+
 print "hello world"
